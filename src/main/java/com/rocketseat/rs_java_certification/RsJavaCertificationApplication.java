2bcbbc1bac4c58dbffb83f098b1d3f8cb0b0955c
Jonas Souza <jonasmzsouza@gmail.com>
2024-02-14 21:13:58 -0300

    project created

---
+++ src/main/java/com/rocketseat/rs_java_certification/RsJavaCertificationApplication.java
@@ -0,0 +1,13 @@
+package com.rocketseat.rs_java_certification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsJavaCertificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsJavaCertificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rocketseat/rs_java_certification/RsJavaCertificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocketseat.rs_java_certification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsJavaCertificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
