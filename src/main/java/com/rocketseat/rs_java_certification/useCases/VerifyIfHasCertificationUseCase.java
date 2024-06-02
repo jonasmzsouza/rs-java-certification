c8f046621f61361f61cfa41a63690880547db3d7
Jonas Souza <jonasmzsouza@gmail.com>
2024-06-02 14:40:32 -0300

    feat: add findByStudentEmailAndTechnology query

--- src/main/java/com/rocketseat/rs_java_certification/useCases/VerifyIfHasCertificationUseCase.java
+++ src/main/java/com/rocketseat/rs_java_certification/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,15 +1,18 @@
 package com.rocketseat.rs_java_certification.useCases;
 
 import com.rocketseat.rs_java_certification.dto.VerifyIfHasCertificationDTO;
+import com.rocketseat.rs_java_certification.repositories.CertificationStudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VerifyIfHasCertificationUseCase {
 
+    @Autowired
+    private CertificationStudentRepository certificationStudentRepository;
+
     public boolean execute(VerifyIfHasCertificationDTO dto) {
-        if (dto.getEmail().equals("email@email.com") && dto.getTechnology().equals("JAVA")) {
-            return true;
-        }
-        return false;
+        var result = this.certificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+        return !result.isEmpty();
     }
 }
